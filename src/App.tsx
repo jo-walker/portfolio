95dcac4ec1ad717ca40453c5379a89832e0fb702
jo gurvantamir <67881725+jo-walker@users.noreply.github.com>
2026-07-07 01:24:01 -0400

    feat: assemble desktop shell in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { WindowManagerProvider } from './window-manager/WindowManagerContext';
+import { Desktop } from './shell/Desktop';
+import { Taskbar } from './shell/Taskbar';
+
 export function App() {
-  return <div className="desktop" />;
+  return (
+    <WindowManagerProvider>
+      <Desktop />
+      <Taskbar />
+    </WindowManagerProvider>
+  );
 }
